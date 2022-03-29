1d006aad40534cf095447d4c65ea6b3c96ca7fd2
Toleraonely <295169157@qq.com>
2022-03-29 15:19:24 +0800

    ICO

---
+++ ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x65cCd1964E418b12d828CFcc91705412D90Fe715";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
